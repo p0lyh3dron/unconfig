9bae6fae4685026b16685a96a2a683376bad0eb1
p0lyh3dron <61992139+p0lyh3dron@users.noreply.github.com>
2020-08-22 17:14:50 -0400

    example: Add comments to be more useful

--- example.cpp
+++ example.cpp
@@ -5,9 +5,13 @@ int main
 {
     Parsed p("exampleconfig.cfg");
 
-    printf("a is %s\n", p.get_value_from_label_in_object(p.nodeHead, "values", "a"));
-    printf("b is %s\n", p.get_value_from_label_in_object(p.nodeHead, "values", "b"));
-    printf("c is %s\n", p.get_value_from_label_in_object(p.nodeHead, "values", "c"));
+    printf("highest value is %s\n\n", p.get_value_from_label_in_object(p.nodeHead, "value information", "highest number")); //  Read the object in the object.
+
+    printf("a is %s\n", p.get_value_from_label_in_object(p.nodeHead, "values", "a"));   //  Find label a and get its value.
+    printf("b is %s\n", p.get_value_from_label_in_object(p.nodeHead, "values", "b"));   //  Find label b and get its value.
+    printf("c is %s\n\n", p.get_value_from_label_in_object(p.nodeHead, "values", "c"));   //  Find label c and get its value.
+
+    p.dump(p.nodeHead);
 
     return 0;
 }
