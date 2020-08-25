4801378ca8d4a33027de05a3336a9ee67f8f274d
karl <karl@ball.fios-router.home>
2020-08-25 14:44:41 -0400

    modified:   c/example.c

--- c/example.c
+++ c/example.c
@@ -1,4 +1,4 @@
-#include "../include/parser.h"
+#include "parser.h"
 
 struct Node;
 struct Parsed;
@@ -8,15 +8,13 @@ int main
 {
     struct Parsed p;
 
-    p = parse("bhop.cfg");
+    p = parse("exampleconfig.cfg");
 
     printf("highest value is %s\n\n", get_value_from_label_in_object(p.nodeHead, "value information", "highest number")); //  Read the object in the object.
 
     printf("a is %s\n", get_value_from_label_in_object(p.nodeHead, "values", "a"));   //  Find label a and get its value.
     printf("b is %s\n", get_value_from_label_in_object(p.nodeHead, "values", "b"));   //  Find label b and get its value.
     printf("c is %s\n\n", get_value_from_label_in_object(p.nodeHead, "values", "c"));   //  Find label c and get its value.
-
-    printf("gmod existing = %i\n", does_object_exist(p.nodeHead, "gmod"));
 
     dump_parsed(p.nodeHead);
     free_parsed(p);
